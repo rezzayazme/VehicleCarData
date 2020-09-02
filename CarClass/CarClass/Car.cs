37ff7b821b8be542c8d0b38b8e4fda61982eda18
rezzayazme <30610229+rezzayazme@users.noreply.github.com>
2020-09-02 16:32:57 -0400

    Add files via upload

---
+++ CarClass/CarClass/Car.cs
@@ -0,0 +1,61 @@
+﻿//Yasser Meza
+//ITCS 3112-051
+//Feb 22, 2019
+//CarClassMain
+
+using System;
+
+namespace CarClass
+{
+    public class Car
+    {
+        //class variables
+        int year_model = 0;
+        string model = "";
+        double speed;
+
+        //Main constructor, requires year and model
+        public Car(int yModel, string cModel)
+            {
+                year_model = yModel;
+                model = cModel;
+                speed = 0.0;
+            }
+         
+        //Accelrator method, adds 5 to speed
+        //returns double for speed
+        public double Accelerator()
+            {
+                speed = speed + 5;
+                return speed;
+            }
+
+        //Brake method, subtracts 5 from speed
+        //return double for speed
+        public double Brake()
+            {
+                speed = speed - 5;
+                return speed;
+            }
+
+        //currentSpeed method, 
+        //returns the value in speed
+        public double currentSpeed()
+            {
+                return speed;
+            }
+
+        //returns model instantiated from Main
+        public string GetModel()
+        {
+            return model;
+        }
+
+        //returns year instantiated from Main
+        public int GetYear()
+        {
+            return year_model;
+        }
+
+    }
+}
---
+++ CarClass/CarClass/MainCarClass.cs
@@ -0,0 +1,106 @@
+﻿//Yasser Meza
+//ITCS 3112-051
+//Feb 22, 2019
+//CarClassMain
+
+using System;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Initializing all main variable
+            string carModel;
+            int carYear;
+            double carSpeed;
+            string carYearS;
+
+            //Starting input and output 
+            //Asking for car demographics
+            //Model
+            Console.WriteLine("What is the Car Model?");
+            carModel = Console.ReadLine();
+
+            //Year
+            Console.WriteLine("What is the Car Year?");
+            carYearS = Console.ReadLine();
+            carYear = Convert.ToInt32(carYearS);
+
+            //Creating Car.cs instance
+            Car car = new Car(carYear, carModel);
+
+
+            //Letting user know that Accelerating method in
+            // Car class is being used
+            Console.WriteLine("");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("ACCELERATING");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("");
+
+
+            //Starting loop for Accelerating loop
+            //increment 5 to speed evertime it is called
+            //looping 5 times
+            int a = 5;
+            while (a >= 0)
+            {
+                //outputting current speed each time accelerating 
+                //method is used.. starting speed should be 0
+                carSpeed = car.currentSpeed();
+                Console.WriteLine("Current Car Speed: " + carSpeed);
+
+                //calling method from Car class
+                car.Accelerator();
+
+                //decrement to stop loop from accelrating
+                a--;
+            }
+
+            //outputting top speed once accelerating stops
+            carSpeed = car.currentSpeed();
+            Console.WriteLine("Current Car Speed: " + carSpeed);
+
+            //Letting user know that Brake method in
+            // Car class is being used
+            Console.WriteLine("");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("BRAKING");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("");
+
+
+            //Starting loop for Brake loop
+            //decrement 5 to speed everytime it is called
+            //looping 5 times
+            int b = 5;
+            while (b >= 0)
+            {
+                //outputting current speed each time accelerating 
+                //method is used.. speed should be top speed from 
+                //accelerating method loop
+                carSpeed = car.currentSpeed();
+                Console.WriteLine("Current Car Speed: " + carSpeed);
+
+                //calling method from Car class to brake the car
+                car.Brake();
+
+                //decrement from b to stop braking loop
+                b--;
+            }
+
+            //Final output after Car has Accelerated and Braked 5 times each 
+            //in succession
+            //Outputs the model and year from the Car class
+            Console.WriteLine("");
+            carSpeed = car.currentSpeed();
+            Console.WriteLine("Current Car Speed: " + carSpeed);
+            Console.WriteLine("Car Model: " + car.GetModel());
+            Console.WriteLine("Car Year: " + car.GetYear());
+        
+        }//end main
+    }//end main class program 
+}//end MainCarClass
